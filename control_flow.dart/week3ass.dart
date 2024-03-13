8317b4c2f1d6a4117e20b3a059e7667acd335c25
Dominick Odhiambo <esco@students.uonbi.ac.ke>
2024-03-14 02:57:30 +0300

    initial commit

---
+++ control_flow.dart/week3ass.dart
@@ -0,0 +1,10 @@
+void main(){
+  var number=15; 
+  if (number > 10) {
+    print("number greater than ten");
+  } else if (number==10) {
+    print("Your number is equal to 10");
+  } else {
+    print ("Your number is less than 10");
+  }
+}
